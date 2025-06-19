72222d828911bbffb7b934705e3bd6ca9f7688c4
AppDevCollab <appdevcollab@gmail.com>
2025-06-19 16:53:40 +0530

    Chamged colour

--- lib/appbar/appbar.dart
+++ lib/appbar/appbar.dart
@@ -19,13 +19,13 @@ class _AppbarState extends State<Appbar> {
           // border: Border.all(color: Colors.black, width: 1),
           boxShadow: [
             BoxShadow(
-              color: const Color.fromARGB(255, 255, 255, 255),
+              color: const Color.fromARGB(255, 0, 0, 0),
               blurRadius: 10,
               spreadRadius: 2,
               offset: Offset(0, 0),
             ),
           ],
-          color: const Color.fromARGB(255, 255, 255, 255),
+          color: const Color.fromARGB(255, 0, 0, 0),
           borderRadius: BorderRadius.circular(10),
         ),
         child: Column(
